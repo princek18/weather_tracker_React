f24d0a9c024002a1f0152b1677248ad219a1dfdc
princek18 <princekumar181999@gmail.com>
2021-08-06 15:23:38 +0530

    First_weather

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Header from './components/Header/Header';
@@ -6,6 +5,8 @@ import Footer from './components/Footer/Footer'
 import Fetch from './components/Fetch/Fetch';
 import Display from './components/Display/Display';
 import Error from './components/Error';
+import axios from 'axios';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 
 function App() {
@@ -18,39 +19,47 @@ function App() {
         return <Error/>;
       }
   }
+
   const [data, setData] = useState([]);
   const [e, setE] = useState("");
+
   const api = (city) => {
-    let n;
-    fetch("http://api.weatherapi.com/v1/current.json?key=f8dafc0bda574e0bb4d154724201308&q=" + city)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          try{
-            let k = {
-              name: result.location.name,
-              state: result.location.region,
-              country: result.location.country,
-              zone: result.location.tz_id,
-              time: result.location.localtime,
-              temp: result.current.temp_c,
-              condition: result.current.condition.text,
-              icon: result.current.condition.icon,
-              speed: result.current.wind_kph,
-              direction: result.current.wind_dir,
-              humidity: result.current.humidity,
-              feels_like: result.current.feelslike_c
-            };
-            setData(k);
-            setE(true);
-          }
-          catch(err){
-            setE(false);
-            console.log("error");
-          }
+    axios.get("http://api.weatherapi.com/v1/current.json?key=f8dafc0bda574e0bb4d154724201308&q=" + city)
+    .then((response) =>{
+      let result = response.data;
+      let {location, current} = result;
+      let {name, region, country, tz_id, localtime} = location;
+      let {temp_c, condition, wind_kph, wind_dir, humidity, feelslike_c} = current;
+      let {text, icon} = condition;
+      let k = {
+        name: name,
+        state: region,
+        country: country,
+        zone: tz_id,
+        time: localtime,
+        temp: temp_c,
+        condition: text,
+        icon: icon,
+        speed: wind_kph,
+        direction: wind_dir,
+        humidity: humidity,
+        feels_like: feelslike_c
+      };
+      setData(k);
+      setE(true);
+    }, (error) =>{
+      setE(false);
+      console.log(error);
+    });
+    // fetch("http://api.weatherapi.com/v1/current.json?key=f8dafc0bda574e0bb4d154724201308&q=" + city)
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       try{
+    //         // let {name, region, country, tz_id, localtime, temp_c, condition, current} = result
+
+    //       }
         }
-      )
-  }
   return (
     <div className="App">
       <Header/>
--- src/components/Fetch/Fetch.jsx
+++ src/components/Fetch/Fetch.jsx
@@ -1,24 +1,76 @@
 import React from 'react'
 import { useState } from 'react'
 import './Fetch.css'
+import { Form, Input, Button, Row, Col} from 'antd';
 
 
 export default function Fetch({api}) {
+  const [form] = Form.useForm();
   const[city, setCity] = useState("")
-  const handlesubmit = (e)=>{
-    e.preventDefault();
-    api(city)
-    setCity("");
+
+  
+  const handlesubmit = ({cityname})=>{
+    // e.preventDefault();
+    console.log(cityname);
+    api(cityname)
+    // setCity("");
+    form.resetFields();
   }
+
+
   const put = (e)=>{
     setCity(e.target.value)
   }
     return (
-        <div>
-          <form action="" onSubmit={handlesubmit}>
-          <input placeholder="Enter City" type="text" value={city} onChange={put} required />
-          <button className="btn btn-sm btn-primary" >Search</button>
-          </form>
-        </div>
+        // <div>
+        //   <form action="" onSubmit={handlesubmit}>
+        //   <input placeholder="Enter City" type="text" value={city} onChange={put} required />
+        //   <button className="btn btn-sm btn-primary" >Search</button>
+        //   </form>
+        // </div>
+        <Row gutter={24}>
+        <Form form={form}
+        name="basic"
+
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={handlesubmit}
+        // onFinishFailed={onFinishFailed}
+      ><Col span={8}>
+        <Form.Item
+          label="Search for city"
+          name="cityname"
+          rules={[
+            {
+              required: true,
+              message: 'Please type a city!',
+            },
+          ]}
+        >
+          <Input/>
+        </Form.Item>
+        </Col>
+  
+        <Col sm={24} md={16} lg={8}>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+
+        >
+        </Form.Item>
+        </Col>
+          
+        <Col xs={24} md={16} xl={8}>
+        <Form.Item
+
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+        </Col>
+      </Form>
+      </Row>
     )
 }
